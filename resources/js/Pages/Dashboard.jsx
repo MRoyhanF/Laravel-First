b2d39bd0639462350eca4d7b1027cead9968a3de
Muhamad Royhan Fadhli <122357726+MRoyhanF@users.noreply.github.com>
2024-01-02 22:25:26 +0700

    part 4.2 24.40

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -7,6 +7,10 @@ export default function Dashboard({ auth }) {
     const [description,setDescription] = useState('');
     const [category,setCategory] = useState('');
 
+    const handleSubmit = () => {
+
+    }
+
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -17,10 +21,10 @@ export default function Dashboard({ auth }) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="p-6 text-gray-900">
-                        <input type="text" placeholder="Judul" className="m-2 input input-bordered w-full" />
-                        <input type="text" placeholder="Deskripsi" className="m-2 input input-bordered w-full" />
-                        <input type="text" placeholder="Kategori" className="m-2 input input-bordered w-full" />
-                        <button className='btn btn-primary m-2'>SUBMIT</button>
+                        <input type="text" placeholder="Judul" className="m-2 input input-bordered w-full" onChange={(title) => setTitle(setTitle.target.value)}/>
+                        <input type="text" placeholder="Deskripsi" className="m-2 input input-bordered w-full" onChange={(description) => setTitle(setDescription.target.value)}/>
+                        <input type="text" placeholder="Kategori" className="m-2 input input-bordered w-full" onChange={(category) => setTitle(setCategory.target.value)}/>
+                        <button className='btn btn-primary m-2' onClick={() => handleSubmit()}>SUBMIT</button>
                     </div>
                 </div>
             </div>
